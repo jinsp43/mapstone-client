50a6b9c7641d7e4ea685ee588294d7ccf31ef574
jinsp43 <jinsp43@gmail.com>
2023-03-28 23:41:51 +0100

    try adding markers to mapbox using layers

--- src/pages/MapPage/MapPage.js
+++ src/pages/MapPage/MapPage.js
@@ -1,4 +1,5 @@
 import "./MapPage.scss";
+import "mapbox-gl/dist/mapbox-gl.css";
 import React, { useRef, useEffect, useState } from "react";
 import mapboxgl from "!mapbox-gl"; // eslint-disable-line import/no-webpack-loader-syntax
 
@@ -13,6 +14,34 @@ const MapPage = () => {
   const [lat, setLat] = useState(51.5263);
   const [zoom, setZoom] = useState(16.2);
 
+  const markers = [
+    {
+      title: "Keu!",
+      longitude: -0.0815,
+      latitude: 51.5266,
+    },
+    {
+      title: "Honi Poke",
+      longitude: -0.0807,
+      latitude: 51.5248,
+    },
+  ];
+
+  const features = markers.map((marker) => {
+    return {
+      type: "Feature",
+      geometry: {
+        type: "Point",
+        coordinates: [marker.longitude, marker.latitude],
+      },
+      properties: {
+        title: marker.title,
+      },
+    };
+  });
+
+  console.log(features);
+
   useEffect(() => {
     if (map.current) return; // initialize map only once
     map.current = new mapboxgl.Map({
@@ -20,6 +49,40 @@ const MapPage = () => {
       style: "mapbox://styles/mapbox/streets-v12",
       center: [lng, lat],
       zoom: zoom,
+    });
+
+    map.current.on("load", () => {
+      // Add an image to use as a custom marker
+      map.current.loadImage(
+        "https://docs.mapbox.com/mapbox-gl-js/assets/custom_marker.png",
+        (error, image) => {
+          if (error) throw error;
+          map.current.addImage("custom-marker", image);
+          // Add a GeoJSON source with 2 points
+          map.current.addSource("points", {
+            type: "geojson",
+            data: {
+              type: "FeatureCollection",
+              features: features,
+            },
+          });
+
+          // Add a symbol layer
+          map.current.addLayer({
+            id: "points",
+            type: "symbol",
+            source: "points",
+            layout: {
+              "icon-image": "custom-marker",
+              // get the title name from the source's "title" property
+              "text-field": ["get", "title"],
+              "text-font": ["Open Sans Semibold", "Arial Unicode MS Bold"],
+              "text-offset": [0, 1.25],
+              "text-anchor": "top",
+            },
+          });
+        }
+      );
     });
   });
 
