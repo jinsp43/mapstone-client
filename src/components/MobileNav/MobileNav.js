fbcc960d0d346b4cb91104b32cb6b761da8ab624
jinsp43 <jinsp43@gmail.com>
2023-03-30 14:08:00 +0100

    adjust font to use variable font and style mobile navbar

--- src/components/MobileNav/MobileNav.js
+++ src/components/MobileNav/MobileNav.js
@@ -8,11 +8,30 @@ import profile from "../../assets/icons/Profile.svg";
 const MobileNav = () => {
   return (
     <nav className="mobile-nav">
-      <img className="mobile-nav__icon" src={people} alt="Groups List" />
-      <img className="mobile-nav__icon" src={marker} alt="Marker" />
-      <img className="mobile-nav__icon" src={add} alt="Add" />
-      <img className="mobile-nav__icon" src={settings} alt="Settings" />
-      <img className="mobile-nav__icon" src={profile} alt="Profile" />
+      <div className="mobile-nav__link">
+        <img className="mobile-nav__icon" src={people} alt="Groups List" />
+        <p className="mobile-nav__text">My Groups</p>
+      </div>
+
+      <div className="mobile-nav__link">
+        <img className="mobile-nav__icon" src={marker} alt="Marker" />
+        <p className="mobile-nav__text">Places</p>
+      </div>
+
+      <div className="mobile-nav__link">
+        <img className="mobile-nav__icon" src={add} alt="Add" />
+        <p className="mobile-nav__text">Add a Place</p>
+      </div>
+
+      <div className="mobile-nav__link">
+        <img className="mobile-nav__icon" src={settings} alt="Settings" />
+        <p className="mobile-nav__text">Settings</p>
+      </div>
+
+      <div className="mobile-nav__link">
+        <img className="mobile-nav__icon" src={profile} alt="Profile" />
+        <p className="mobile-nav__text">My Profile</p>
+      </div>
     </nav>
   );
 };
