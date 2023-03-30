e0ff1d7416b354871d2edd3686452f232e4e3422
jinsp43 <jinsp43@gmail.com>
2023-03-30 12:38:50 +0100

    create and style header for all breakpoints

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.scss";
+import Header from "./components/Header/Header";
 import MapPage from "./pages/MapPage/MapPage";
 
 const App = () => {
-  return <div className="App">{/* <MapPage /> */}</div>;
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="map" element={<MapPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,16 @@
 import "./Header.scss";
+import logo from "../../assets/logo/Logo.svg";
+import people from "../../assets/icons/People.svg";
+import profile from "../../assets/icons/Profile.svg";
 
 const Header = () => {
-  return <div></div>;
+  return (
+    <header className="header">
+      <img className="header__icon" src={people} alt="Groups" />
+      <img className="header__logo" src={logo} alt="Our Places Logo" />
+      <img className="header__icon" src={profile} alt="Profile" />
+    </header>
+  );
 };
 
 export default Header;
