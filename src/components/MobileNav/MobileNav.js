b49e948e1bbceb721ee5212c199f17d4a770e209
jinsp43 <jinsp43@gmail.com>
2023-04-04 14:32:43 +0100

    setup links for mobile nav bar and remove underlines

--- src/components/MobileNav/MobileNav.js
+++ src/components/MobileNav/MobileNav.js
@@ -4,34 +4,35 @@ import marker from "../../assets/icons/Marker.svg";
 import add from "../../assets/icons/Add.svg";
 import settings from "../../assets/icons/Settings.svg";
 import profile from "../../assets/icons/Profile.svg";
+import { Link } from "react-router-dom";
 
 const MobileNav = ({ addPlace }) => {
   return (
     <nav className="mobile-nav">
-      <div className="mobile-nav__link">
+      <Link to="/groups" className="mobile-nav__link">
         <img className="mobile-nav__icon" src={people} alt="Groups List" />
         <p className="mobile-nav__text">My Groups</p>
-      </div>
+      </Link>
 
-      <div className="mobile-nav__link">
+      <Link to="/places" className="mobile-nav__link">
         <img className="mobile-nav__icon" src={marker} alt="Marker" />
         <p className="mobile-nav__text">Places</p>
-      </div>
+      </Link>
 
       <div onClick={addPlace} className="mobile-nav__link">
         <img className="mobile-nav__icon" src={add} alt="Add" />
         <p className="mobile-nav__text">Add a Place</p>
       </div>
 
-      <div className="mobile-nav__link">
+      <Link to="/settings" className="mobile-nav__link">
         <img className="mobile-nav__icon" src={settings} alt="Settings" />
         <p className="mobile-nav__text">Settings</p>
-      </div>
+      </Link>
 
-      <div className="mobile-nav__link">
+      <Link to="/profile" className="mobile-nav__link">
         <img className="mobile-nav__icon" src={profile} alt="Profile" />
         <p className="mobile-nav__text">My Profile</p>
-      </div>
+      </Link>
     </nav>
   );
 };
