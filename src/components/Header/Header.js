47a7f62a85650fdc55e28c06cd798a4fb24f67b7
jinsp43 <jinsp43@gmail.com>
2023-04-12 22:27:24 +0100

    make logo link to landing page

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,12 +2,15 @@ import "./Header.scss";
 import logo from "../../assets/logo/Logo.svg";
 import people from "../../assets/icons/People.svg";
 import profile from "../../assets/icons/Profile.svg";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <header className="header">
       <img className="header__icon" src={people} alt="Groups" />
-      <img className="header__logo" src={logo} alt="Our Places Logo" />
+      <Link to="/">
+        <img className="header__logo" src={logo} alt="Our Places Logo" />
+      </Link>
       <img className="header__icon" src={profile} alt="Profile" />
     </header>
   );
